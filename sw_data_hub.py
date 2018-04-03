c2323d6119e5442043e11cfa65d1b42b994cc247
Andrey.Logunov <logunov@hotmail.com>
2018-04-03 17:37:57 -0400

    2018-04-03

--- sw_data_hub.py
+++ sw_data_hub.py
@@ -4,21 +4,26 @@ class DataHub:
     def __init__(self, stocks, history, study, fundamentals):
         self._technical = {}
         self._fundamentals = {}
-
         for stock in stocks:
-            hdf = history.to_dataframe(stock)
-            if hdf is not None:
-                sdf = study.to_dataframe(stock)
-                if sdf is not None:
-                    hdf = hdf.join(sdf)
-            self._technical[stock] = hdf
+            self._technical[stock] = {}
+            self._technical[stock]['history'] = history.to_dataframe(stock)
+            self._technical[stock]['study'] = study.to_dataframe(stock)
             self._fundamentals[stock] = {}
             self._fundamentals[stock]['financials'] = fundamentals.financials(stock)
             self._fundamentals[stock]['peers'] = fundamentals.peers(stock)
             self._fundamentals[stock]['stats'] = fundamentals.stats(stock)
 
-    def technical(self):
-        return self._technical
+    def history(self, stock):
+        return self._technical[stock]['history']
 
-    def fundamentals(self):
-        return self._fundamentals
+    def study(self, stock):
+        return self._technical[stock]['study']
+
+    def financials(self, stock):
+        return self._fundamentals[stock]['financials']
+
+    def peers(self, stock):
+        return self._fundamentals[stock]['peers']
+
+    def stats(self, stock):
+        return self._fundamentals[stock]['stats']
