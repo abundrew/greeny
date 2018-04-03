868246f3ad87b1f627ce5ef3f2c710d12d54a8d5
abundrew <logunov@hotmail.com>
2018-04-02 21:13:15 -0400

    2018-04-01

--- sw_fundamentals.py
+++ sw_fundamentals.py
@@ -9,59 +9,64 @@ import sw_stock
 
 class Fundamentals:
     def __init__(self,
-                 stocks = None,
                  path_financials=sw_config.PATH_FINANCIALS,
                  path_peers=sw_config.PATH_PEERS,
                  path_stats=sw_config.PATH_STATS):
-        self._stocks = stocks
-        if stocks is None:
-            stock = sw_stock.Stock()
-            self._stocks = stock.stocks()
         self._path_financials = path_financials
         self._path_peers = path_peers
         self._path_stats = path_stats
 
+        fname = self._path_financials
+        if not os.path.isfile(fname):
+            self.update()
+        with open(fname) as f:
+            self._financials = json.load(f)
+
+        fname = self._path_peers
+        if not os.path.isfile(fname):
+            self.update()
+        with open(fname) as f:
+            self._peers = json.load(f)
+
+        fname = self._path_stats
+        if not os.path.isfile(fname):
+            self.update()
+        with open(fname) as f:
+            self._stats = json.load(f)
+
     def financials(self, stock):
         try:
-            fname = self._path_financials
-            if not os.path.isfile(fname):
-                financials = iex_api.stock_batch_100(self._stocks, 'financials')
-                s = json.dumps(financials)
-                with open(fname, 'w') as f:
-                    f.write(s)
-            with open(fname) as f:
-                financials = json.load(f)
-            return financials['data'][stock]['financials']['financials']
+            return self._financials['data'][stock]['financials']['financials']
         except:
             error = "ERROR: {} {}".format(sys.exc_info()[0], sys.exc_info()[1])
             print(error)
 
     def peers(self, stock):
         try:
-            fname = self._path_peers
-            if not os.path.isfile(fname):
-                peers = iex_api.stock_batch_100(self._stocks, 'peers')
-                s = json.dumps(peers)
-                with open(fname, 'w') as f:
-                    f.write(s)
-            with open(fname) as f:
-                peers = json.load(f)
-            return peers['data'][stock]['peers']
+            return self._peers['data'][stock]['peers']
         except:
             error = "ERROR: {} {}".format(sys.exc_info()[0], sys.exc_info()[1])
             print(error)
 
     def stats(self, stock):
         try:
-            fname = self._path_stats
-            if not os.path.isfile(fname):
-                stats = iex_api.stock_batch_100(self._stocks, 'stats')
-                s = json.dumps(stats)
-                with open(fname, 'w') as f:
-                    f.write(s)
-            with open(fname) as f:
-                stats = json.load(f)
-            return stats['data'][stock]['stats']
+            return self._stats['data'][stock]['stats']
         except:
             error = "ERROR: {} {}".format(sys.exc_info()[0], sys.exc_info()[1])
             print(error)
+
+    def update(self):
+        stocks = sw_stock.Stock()
+        component = {'financials':self._path_financials, 'peers':self._path_peers, 'stats':self._path_stats}
+        for key in component:
+            fname = component[key]
+            bname = fname + '.bak'
+            if os.path.isfile(fname):
+                if os.path.isfile(bname):
+                    os.remove(bname)
+                os.rename(fname, fname + '.bak')
+            data = iex_api.stock_batch_100(stocks, key)
+            s = json.dumps(data)
+            with open(fname, 'w') as f:
+                f.write(s)
+
